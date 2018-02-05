8c6d41b7364bbf5514f6844f028e29ac588c4ab8
vincent <omegaortiz1997@gmail.com>
2018-02-05 00:53:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function sayHiToGrandma(string) {
     return string.toLowerCase("I can\'t hear you!");
   } else if (string === string.toUpperCase()) {
     return string.toUpperCase ("YES INDEED!");
-  } else (string = mixedCase); {
+  } else (string = "I love you, Grandma."); {
     return ("I love you, too.");
   }
 }
